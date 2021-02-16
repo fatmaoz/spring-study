d9f23f0393a353dab7c708a622c7f444765ca486
fatmaoz <fatmas199021@gmail.com>
2021-02-16 18:04:52 +0000

    Spring REST-Template

---
+++ Spring-11-Rest-RestTemplate/src/main/java/com/cybertek/ResttemplateApplication.java
@@ -0,0 +1,27 @@
+package com.cybertek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.Duration;
+
+@SpringBootApplication
+public class ResttemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResttemplateApplication.class, args);
+	}
+
+	//Bu bean APi consuming icin
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder){
+		return builder
+				.setConnectTimeout(Duration.ofMillis(3000))
+				.setReadTimeout(Duration.ofMillis(3000))
+				.build();
+	}
+}
